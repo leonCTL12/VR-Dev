68f61f8d38670221a102671539c75041da9d067d
leonctl <leonchantinglok@gmail.com>
2021-09-03 10:08:20 +0800

    added hand with physics, hands and teleportation XR Rig prefab

--- VR Development/Assets/Tutorial/Scripts/Hand.cs
+++ VR Development/Assets/Tutorial/Scripts/Hand.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
+    //Animation
     Animator animator;
     SkinnedMeshRenderer mesh;
     private float gripTarget;
@@ -14,21 +15,62 @@ public class Hand : MonoBehaviour
     private float triggerCurrent;
     private string animatorGripParam = "Grip";
     private string animatorTriggerParam = "Trigger";
-
     [SerializeField]
-    private float speed;
+    private float animationSpeed;
 
-    // Start is called before the first frame update
+    //Physics Movement
+    [SerializeField]
+    private GameObject followObject;
+    [SerializeField]
+    private float followSpeed = 30f;
+    [SerializeField]
+    private float rotateSpeed = 100f;
+    [SerializeField] 
+    private Vector3 positionOffset;
+    [SerializeField]
+    private Vector3 rotationOffset;
+
+
+    private Transform _followTarget;
+    private Rigidbody _body;
+
     void Start()
     {
+        // Animation
         animator = GetComponent<Animator>();
         mesh = GetComponentInChildren<SkinnedMeshRenderer>();
+
+        //Physics Movement
+        _followTarget = followObject.transform;
+        _body = GetComponent<Rigidbody>();
+        _body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        _body.interpolation = RigidbodyInterpolation.Interpolate;
+        _body.mass = 20f;
+
+        //Teleport hands
+        _body.position = _followTarget.position;
+        _body.rotation = _followTarget.rotation;
     }
 
-    // Update is called once per frame
     void Update()
     {
         AnimateHand();
+
+        PhysicsMove();
+    }
+
+    private void PhysicsMove()
+    {
+        // Position
+        var positionWithOffset = _followTarget.position + positionOffset;
+        var distance = Vector3.Distance(positionWithOffset, transform.position);
+        _body.velocity = (positionWithOffset - transform.position).normalized * (followSpeed * distance);
+
+        //Rotation
+        var rotationWithOffset = _followTarget.rotation * Quaternion.Euler(rotationOffset);
+        var quaternionDifference = rotationWithOffset * Quaternion.Inverse(_body.rotation); //Find the difference in rotation between the controller and hand model
+        quaternionDifference.ToAngleAxis(out float angle, out Vector3 axis);
+        _body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
     }
 
     internal void SetGrip(float v)
@@ -45,12 +87,12 @@ public class Hand : MonoBehaviour
     {
         if (gripCurrent != gripTarget)
         {
-            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * animationSpeed);
             animator.SetFloat(animatorGripParam, gripCurrent);
         }
         if (triggerCurrent != triggerTarget)
         {
-            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * animationSpeed);
             animator.SetFloat(animatorTriggerParam, triggerCurrent);
         }
     }
---
+++ VR Development/Assets/Tutorial/Scripts/TeleportationManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportationManager : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionAsset actionAsset;
+    [SerializeField]
+    private XRRayInteractor rayInteractor;
+    [SerializeField]
+    private TeleportationProvider provider;
+
+    private InputAction _thumbstick;
+    private bool _isActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayInteractor.enabled = false; //disable the ray before the player push the stick
+
+        var activate = actionAsset.FindActionMap("XRI LeftHand").FindAction("Teleport Mode Activate");
+        activate.Enable();
+        activate.performed += OnTeleportActivate;
+
+        var cancel = actionAsset.FindActionMap("XRI LeftHand").FindAction("Teleport Mode Cancel");
+        cancel.Enable();
+        cancel.performed += OnTeleportationCancel;
+
+        _thumbstick = actionAsset.FindActionMap("XRI LeftHand").FindAction("Move");
+        _thumbstick.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_isActive)
+            return;
+
+        if (_thumbstick.triggered) //the stick is still pushing forward, that means the player do not want to teleport yet.
+            return;
+
+        if(!rayInteractor.GetCurrentRaycastHit(out RaycastHit hit)) //the ray is hitting something non existed
+        {
+            rayInteractor.enabled = false;
+            _isActive = false;
+            return;
+        }
+
+        TeleportRequest request = new TeleportRequest()
+        {
+            destinationPosition = hit.point,
+            //destinationRotation = ,
+            //matchOrientation = ,
+            //requestTime = ,
+        };
+
+        provider.QueueTeleportRequest(request);
+        rayInteractor.enabled = false;
+        _isActive = false;
+    }
+
+    private void OnTeleportActivate(InputAction.CallbackContext context)
+    {
+        rayInteractor.enabled = true;
+        _isActive = true;
+    }
+
+    private void OnTeleportationCancel(InputAction.CallbackContext context)
+    {
+        rayInteractor.enabled = false;
+        _isActive = false;
+    }
+}
