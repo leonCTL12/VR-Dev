4eb3e15d088a8a474fc2bf53a21095babc0a8cbe
leonctl <leonchantinglok@gmail.com>
2022-01-24 18:29:10 +0800

    added weak spot on hit vfx

--- VR Development/Assets/Scripts/Level Boss Fight/Boss/WeakSpot.cs
+++ VR Development/Assets/Scripts/Level Boss Fight/Boss/WeakSpot.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class WeakSpot : MonoBehaviour
 {
     [SerializeField]
+    private GameObject normalParticle;
+    [SerializeField]
+    private GameObject onHitParticle;
+    private PhotonView photonView;
     private Boss boss;
+
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
 
     private void Start()
     {
         boss = Boss.BossInstance;
+        normalParticle.SetActive(true);
+        onHitParticle.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -19,7 +31,23 @@ public class WeakSpot : MonoBehaviour
             Debug.Log("WeakSpots Hit!");
             //TODO: inform boss that its weak spot is hit
             boss.OnWeakSpotHit();
-            Destroy(gameObject);
+            photonView.RPC("OnDestoryWeakSpot", RpcTarget.All);
         }
     }
+    
+    private IEnumerator DestroyWeakSpotCoroutine()
+    {
+        normalParticle.SetActive(false);
+        onHitParticle.SetActive(true);
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+
+    [PunRPC]
+    private void OnDestoryWeakSpot()
+    {
+        GetComponent<BoxCollider>().enabled = false; //to prevent hitting a collider multiple time;
+        StartCoroutine(DestroyWeakSpotCoroutine());
+    }
+
 }
